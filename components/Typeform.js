d0004622281f6811a15c67af0d4578cae79390c7
vissjustinj <66446045+vissjustinj@users.noreply.github.com>
2022-10-10 19:32:48 -0500

    jotform iframe

--- components/Typeform.js
+++ components/Typeform.js
@@ -4,7 +4,13 @@ function Typeform() {
 	return (
 		<div style={{ height: '100%' }}>
 			<h2>The Survey</h2>
-			<Widget id="AzkmxzSB" style={{ width: '100%', height: '90%' }} className="my-form" />
+			<iframe
+				width="90%"
+				height="90%"
+				id="getstartedvid"
+				src="https://form.jotform.com/222717107340144"
+				frameBorder="0"
+			/>
 		</div>
 	);
 }
